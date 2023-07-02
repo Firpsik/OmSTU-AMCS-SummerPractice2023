7db5338768e3322e51034c16dca583b7f22795d3
dmitry kozlov <dimagg51@mail.ru>
2023-07-02 17:52:20 +0000

    Some text

---
+++ bdd_test/Drivers/Driver.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace bdd_test.Drivers
+{
+    public class Driver
+    {
+
+    }
+}
---
+++ bdd_test/Hooks/Hook.cs
@@ -0,0 +1,11 @@
+using System;
+using TechTalk.SpecFlow;
+
+namespace bdd_test.Hooks
+{
+    [Binding]
+    public class Hooks
+    {
+        
+    }
+}
